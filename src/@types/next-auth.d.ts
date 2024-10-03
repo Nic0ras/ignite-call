b58833c4b71b02f34292d23c1cb6ec967c03b3e4
Nicollas <nicollai2006@gmail.com>
2024-10-03 20:00:18 -0300

    feature: cria componentes e estiliza

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,13 @@
+// arquivo específico para definição de tipos
+
+import NextAuth from 'next-auth/next'
+
+declare module 'next-auth' {
+  export interface User {
+    id: string
+    name: string
+    email: string
+    username: string
+    avatar_url: string
+  }
+}
